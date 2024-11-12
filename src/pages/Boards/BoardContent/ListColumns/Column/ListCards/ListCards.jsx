c21aea66732b557f3711b5a62ae3ede18f5888fe
hoaidzvcl <hoaixoai268@gmail.com>
2024-11-12 15:52:00 +0700

    drag drop card

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -1,31 +1,34 @@
 import React from 'react'
 import Box from '@mui/material/Box'
 import Card from './Card/Card'
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 
-function ListCards({cards}) {
+function ListCards({ cards }) {
     return (
-        <Box
-            sx={{
-                p: '0 4px',
-                m: '0 5px',
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 1,
-                p: 1,
-                overflowX: 'hidden',
-                overflowY: 'auto',
-                maxHeight: (theme) => `calc(
+        <SortableContext items={cards.map(c => c._id)} strategy={verticalListSortingStrategy}>
+            <Box
+                sx={{
+                    p: '0 4px',
+                    m: '0 5px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 1,
+                    p: 1,
+                    overflowX: 'hidden',
+                    overflowY: 'auto',
+                    maxHeight: (theme) => `calc(
                         ${theme.hello.boardContentHeight} - 
                         ${theme.spacing(3)} -
                         ${theme.hello.columnHeaderHeight} -
                         ${theme.hello.columnFooterHeight}
                         )`,
-                '::-webkit-scrollbar-thumb': { backgroundColor: '#ced0da' },
-                '::-webkit-scrollbar-thumb:hover': { backgroundColor: '#bfc2cf' }
-            }}
-        >   
-            {cards.map(card => <Card key={card._id} card={card}/>)}
-        </Box>
+                    '::-webkit-scrollbar-thumb': { backgroundColor: '#ced0da' },
+                    '::-webkit-scrollbar-thumb:hover': { backgroundColor: '#bfc2cf' }
+                }}
+            >
+                {cards.map(card => <Card key={card._id} card={card} />)}
+            </Box>
+        </SortableContext>
     )
 }
 
