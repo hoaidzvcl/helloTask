e31beca058b82de272827c318d3437430f0c72ea
hoaidzvcl <hoaixoai268@gmail.com>
2024-11-23 18:02:01 +0700

    routes register login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import * as React from 'react'
+import Auth from '~/pages/Auth/Auth'
 import Board from './pages/Boards/_id'
-
+import NotFound from '~/pages/404/NotFound'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-    <Board />
-    </>
+    <Routes>
+      {/* Redirect Routes */}
+      <Route path='/' element={
+        <Navigate to="/boards/673794d4270b33d675b1c729" replace={true} />
+      }
+      />
+
+      {/* Board Details */}
+      <Route path='/boards/:boardId' element={<Board />} />
+
+      {/* Authentication */}
+      <Route path='/login' element={<Auth />} />
+      <Route path='/register' element={<Auth />} />
+
+      {/* 404 */}
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   )
 }
 
